1e643e5ba625b6ca95e62878e91c2db1c603605a
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-12-12 12:02:45 +0000

    Añadido BookMarkDao

---
+++ app/src/main/java/dev/samu/bladerecycle/data/BookMarkDao.kt
@@ -0,0 +1,15 @@
+package dev.samu.bladerecycle.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BookmarkDao {
+    @Query("SELECT * FROM bookmark_table")
+    fun getAllBookmarks(): Flow<List<Bookmark>>
+
+    @Insert
+    suspend fun insert(bookmark: Bookmark)
+}
