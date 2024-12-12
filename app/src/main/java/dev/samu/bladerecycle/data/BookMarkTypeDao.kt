4ca8cda05447b1bb4326e542fb4548f9e2d1b742
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-12-12 12:04:32 +0000

    Añadido BookMarkTypeDao

---
+++ app/src/main/java/dev/samu/bladerecycle/data/BookMarkTypeDao.kt
@@ -0,0 +1,15 @@
+package dev.samu.bladerecycle.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BookmarkTypeDao {
+    @Query("SELECT * FROM bookmark_type_table")
+    fun getAllBookmarkTypes(): Flow<List<BookmarkType>>
+
+    @Insert
+    suspend fun insert(bookmarkType: BookmarkType)
+}
