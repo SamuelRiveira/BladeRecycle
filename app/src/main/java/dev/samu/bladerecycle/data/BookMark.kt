dd2dba61d3d2c605a0205770961e19d35bc42b53
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-12-12 12:01:40 +0000

    Añadido BookMark

---
+++ app/src/main/java/dev/samu/bladerecycle/data/BookMark.kt
@@ -0,0 +1,13 @@
+package dev.samu.bladerecycle.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bookmark_table")
+data class Bookmark(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val coordinatesX: Double,
+    val coordinatesY: Double,
+    val typeId: Int
+)
